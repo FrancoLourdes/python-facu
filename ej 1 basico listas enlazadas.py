a804b616d2f83c36493b94dd23a708ce4402bc9f
FrancoLourdes <139924445+FrancoLourdes@users.noreply.github.com>
2025-09-15 21:20:07 -0300

    Add files via upload

---
+++ ej 1 basico listas enlazadas.py
@@ -0,0 +1,14 @@
+numeros = ["1", "2", "3", "4", "5"]
+
+print ("La lista es:")
+for numero in numeros:
+    print (numero)
+print ()
+print ("El primer elemento es:")
+print (numeros[0])
+print ()
+print ("El último elemento es:")
+print (numeros[-1])
+print ()
+print ("La longitud de la lista es:")
+print (len(numeros))
