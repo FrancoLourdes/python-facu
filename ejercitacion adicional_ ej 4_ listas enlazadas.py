989214089d171ff149217ab0e2f59aea5b9c6271
FrancoLourdes <139924445+FrancoLourdes@users.noreply.github.com>
2025-09-15 23:31:46 -0300

    Add files via upload

---
+++ ejercitacion adicional_ ej 4_ listas enlazadas.py
@@ -0,0 +1,9 @@
+#Dada la lista letras = ["a", "b", "c", "d", "e", "f"]: Mostrar los primeros 3 elementos, Mostrar los últimos 2 y Mostrar la lista en orden 
+
+letras = ["a", "b", "c", "d", "e", "f"]
+
+print ("Las primeras tres letras son:",letras[:2]) #slicing
+print()
+print ("Las ultimas dos letras son:", letras[4:])
+print()
+print ("La lista es:", letras)
---
+++ ejercitacion adicional_ej3_listas enlazadas.py
@@ -0,0 +1,7 @@
+#Dada la lista numeros = [5, 2, 9, 1, 7], ordenar la lista en orden ascendente y mostrarla.
+
+numeros = ["5", "2", "9", "1", "7"]
+print ("La lista  es:", numeros)
+
+numeros.sort()
+print ("La lista ordenada es:", numeros)
